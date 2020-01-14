a5c25b69f162f926bb5e9878e1df44a07cf0d49d
Jud White <github@judsonwhite.com>
2020-01-14 02:26:08 -0600

    cmd/html2xterm: extract parseArgs func

--- cmd/html2xterm/main.go
+++ cmd/html2xterm/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"flag"
 	"fmt"
 	"io/ioutil"
@@ -11,29 +12,22 @@ import (
 	"github.com/judwhite/html2xterm"
 )
 
+type options struct {
+	js          bool
+	centerWidth int
+	filenames   []string
+}
+
 func main() {
-	var flagSet flag.FlagSet
-	flagXTermJS := flagSet.Bool("js", false, "output xterm.js code")
-	flagWidth := flagSet.Int("width", 0, "center using width")
-	err := flagSet.Parse(os.Args[1:])
+	opts, err := parseArgs(os.Args[1:])
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("%v\nusage: html2xterm [OPTIONS] <filename1> <filename2> ...", err)
 	}
 
-	filenames := flagSet.Args()
-
-	if len(filenames) == 0 {
-		flagSet.PrintDefaults()
-		log.Fatal("usage: html2xterm [OPTIONS] <filename1> <filename2> ...")
-	}
-
-	centerWidth := *flagWidth
-	js := *flagXTermJS
-
-	outputs := make([]html2xterm.Output, len(filenames))
-
-	for i := 0; i < len(filenames); i++ {
-		filename := filenames[i]
+	// parse files
+	outputs := make([]html2xterm.Output, len(opts.filenames))
+	for i := 0; i < len(opts.filenames); i++ {
+		filename := opts.filenames[i]
 		b, err := ioutil.ReadFile(filename)
 		if err != nil {
 			log.Fatal(err)
@@ -47,12 +41,13 @@ func main() {
 		outputs[i] = output
 	}
 
-	if centerWidth != 0 {
-		outputs = html2xterm.Center(centerWidth, outputs...)
+	if opts.centerWidth != 0 {
+		outputs = html2xterm.Center(opts.centerWidth, outputs...)
 	}
 
+	// create output
 	var sb strings.Builder
-	if js {
+	if opts.js {
 		sb.WriteString("const ansi = \"")
 	}
 
@@ -61,16 +56,39 @@ func main() {
 			sb.WriteString(`\r\n`)
 		}
 
-		if js {
+		if opts.js {
 			sb.WriteString(output.JS())
 		} else {
 			fmt.Print(output.ANSI())
 		}
 	}
 
-	if js {
+	if opts.js {
 		sb.WriteString("\";\n\nterm.writeln(ansi);\n")
 	}
 
 	fmt.Printf("%s", sb.String())
+}
+
+func parseArgs(args []string) (options, error) {
+	var flagSet flag.FlagSet
+	flagXTermJS := flagSet.Bool("js", false, "output xterm.js code")
+	flagWidth := flagSet.Int("width", 0, "center using width")
+	err := flagSet.Parse(args)
+	if err != nil {
+		flagSet.PrintDefaults()
+		return options{}, err
+	}
+
+	filenames := flagSet.Args()
+	if len(filenames) == 0 {
+		flagSet.PrintDefaults()
+		return options{}, errors.New("no filename(s) specified")
+	}
+
+	return options{
+		js:          *flagXTermJS,
+		centerWidth: *flagWidth,
+		filenames:   flagSet.Args(),
+	}, nil
 }
